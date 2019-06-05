e368f71a4fdbca125b223c76a610a0d92c0cfc21
Sema <sema@pyva.net>
2019-06-05 00:26:36 -0400

    Updated dependencies

--- javascript/JZZ.input.Kbd.js
+++ javascript/JZZ.input.Kbd.js
@@ -13,7 +13,7 @@
   if (!JZZ) return;
   if (!JZZ.input) JZZ.input = {};
 
-  var _version = '1.0.8';
+  var _version = '1.0.9';
   function _name(name, deflt) { return name ? name : deflt; }
 
   function _copy(obj) {
