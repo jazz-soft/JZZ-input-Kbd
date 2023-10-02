304c360a7dfb7fb6c38f25cf702497d5ebc6ef9e
Sema <jazz-dev@jazz-soft.net>
2023-10-02 00:30:40 -0400

    Updated dependencies

--- javascript/JZZ.input.Kbd.js
+++ javascript/JZZ.input.Kbd.js
@@ -14,7 +14,7 @@
   if (!JZZ) return;
   if (!JZZ.input) JZZ.input = {};
 
-  var _version = '1.2.7';
+  var _version = '1.2.8';
   function _name(name, deflt) { return name ? name : deflt; }
 
   function _copy(obj) {
